81d9ae2f1c40cd4e1d5901850d4621f2c53ed862
Daniel Albanez <danielalbanez40@gmail.com>
2022-07-20 13:05:59 -0300

    Renderizado dinámico de cards

---
+++ app/components/PostCard.js
@@ -0,0 +1,18 @@
+export function PostCard(props) {
+
+    const {title, date, slug, featured_media_src_url} = props;
+
+    let dateformat = new Date(date).toLocaleString();
+    let urlPoster = featured_media_src_url ? featured_media_src_url : 'app/assets/no-image.png';
+
+    return `
+        <article class="post-card">
+            <img src="${urlPoster}" alt="${title.rendered}" />
+            <h2>${title.rendered}</h2>
+            <p>
+                <time datetime="">Fecha: ${dateformat}</time>
+                <a href="#/${slug}" class="publicacion">Ver Publicación</a>
+            </p>
+        </article>
+    `;
+}
