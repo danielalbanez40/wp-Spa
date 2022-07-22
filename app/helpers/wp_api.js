2ddb4992e630ee58eba768fe39a92b39ca073967
Daniel Albanez <danielalbanez40@gmail.com>
2022-07-22 17:29:34 -0300

    Implementando infinite scroll

--- app/helpers/wp_api.js
+++ app/helpers/wp_api.js
@@ -4,18 +4,23 @@ const NAME_SITE = "css-tricks",
     DOMAIN      = `https://${NAME_SITE}.com`,
     SITE        = `${DOMAIN}/wp-json`,
     API_WP      = `${SITE}/wp/v2`,
-    POSTS       = `${API_WP}/posts?_embed`,
+    PER_PAGE    = 10,
+    POSTS       = `${API_WP}/posts?_embed&per_page=${PER_PAGE}`,
     POST        = `${API_WP}/posts`,
-    SEARCH      = `${API_WP}/search?_embed&search=`,
-    CATEGORIES  = `${API_WP}/categories`;
+    SEARCH      = `${API_WP}/search?_embed&per_page=${PER_PAGE}&search=`,
+    CATEGORIES  = `${API_WP}/categories`
+
+    let page = 1;
 
     export default {
         NAME_SITE,
         DOMAIN,
         SITE,
         API_WP,
+        PER_PAGE,
         POSTS,
         POST,
         SEARCH,
-        CATEGORIES
+        page,
+        CATEGORIES,
     }
